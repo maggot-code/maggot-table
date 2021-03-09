8027c6c1610b90a8dd2096b885fc155e9e79f01f
maggot_code <wowhuanhuan233@163.com>
2021-03-09 20:58:28 +0800

    feat：import element component
    1.新增插件引入文件，导入element组件库
    2.导入button组件，button group组件
    3.导入table组件，table column组件
    4.导入pagination 分页器组件

---
+++ plugins/element.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: maggot-code
+ * @Date: 2021-03-03 17:36:24
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2021-03-09 15:52:51
+ * @Description: file content
+ */
+import 'element-ui/lib/theme-chalk/index.css';
+import Vue from 'vue';
+import {
+    // Basic 基本
+    Button,
+    ButtonGroup,
+    // Form 表单
+    // Data 数据
+    Table,
+    TableColumn,
+    Pagination
+    // Notice 通知
+    // Navigation 导航
+    // Others 其他
+} from 'element-ui';
+
+// Basic 基本
+Vue.use(Button);
+Vue.use(ButtonGroup);
+// Form 表单
+// Data 数据
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Pagination);
+// Notice 通知
+// Navigation 导航
+// Others 其他
