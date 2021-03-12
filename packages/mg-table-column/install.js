c0069f27cab111e4c3014e2e4e107f14e97cae8b
maggot_code <wowhuanhuan233@163.com>
2021-03-12 13:52:14 +0800

    feat：add mg-column-link component
    1.新增mg-column-link 组件

--- packages/mg-table-column/install.js
+++ packages/mg-table-column/install.js
@@ -2,13 +2,15 @@
  * @Author: maggot-code
  * @Date: 2021-03-09 15:07:40
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-03-09 15:47:25
+ * @LastEditTime: 2021-03-12 13:28:14
  * @Description: unify install form components
  */
 import MgColumnDefault from '../mg-column-default';
+import MgColumnLink from '../mg-column-link';
 
 const components = [
     MgColumnDefault,
+    MgColumnLink
 ];
 
 const TableColumnComponents = {};
